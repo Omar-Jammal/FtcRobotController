5900931a914b7694cef1a664cbecb814a424ae37
Omar-Jammal <o.jammal46@gmail.com>
2025-09-23 16:33:26 -0500

    Update GamePadPractice.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/GamePadPractice.java
@@ -2,9 +2,8 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 
-@Disabled
+
 @TeleOp
 public class GamePadPractice extends OpMode {
 
