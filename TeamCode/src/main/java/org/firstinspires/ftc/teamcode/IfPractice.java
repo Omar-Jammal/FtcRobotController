19b3f5f6b06b1f7173e1847e4ba87d9a1a42f02d
Omar-Jammal <o.jammal46@gmail.com>
2025-09-26 23:19:19 -0500

    Created more practice programs

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfPractice.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/IfPractice.java
@@ -12,25 +12,27 @@ public class IfPractice extends OpMode{
 
     @Override
     public void loop() {
+        boolean aButton = gamepad1.a;
         double leftY = gamepad1.left_stick_y;
-        if (leftY < 0) {
-            telemetry.addData("Left Stick Y", "Is Negative");
-        }
-        else if (leftY > 0.5){
-            telemetry.addData("Left Stick Y", "Greater than 50%");
-        }
-        else if (leftY > 0) {
-            telemetry.addData("Left Stick Y", "Is greater than 0");
-        }
-        else {
-            telemetry.addData("Left Stick Y", "Is Zero");
-        }
-        telemetry.addData("Left Stick Y", leftY);
+        double motorSpeed = gamepad1.left_stick_y;
 
+
+        if (!aButton) {
+            motorSpeed *= 0.5;
+        }
+
+        telemetry.addData("Left Stick Y", leftY);
+        telemetry.addData("A Button State", aButton);
+        telemetry.addData("Motor Speed", motorSpeed);
 
     }
 
 }
+
+
+
+
+
 
    /*A button check
 
@@ -42,6 +44,26 @@ public class IfPractice extends OpMode{
             telemetry.addData("A button", "Not Pressed");
         }
 
-        telemetry.addData("A Button State", aButton);
+
 
         */
+
+
+    /* Y stick check
+
+    if (leftY < 0) {
+                telemetry.addData("Left Stick Y", "Is Negative");
+            }
+            else if (leftY > 0.5){
+                telemetry.addData("Left Stick Y", "Greater than 50%");
+            }
+            else if (leftY > 0) {
+                telemetry.addData("Left Stick Y", "Is greater than 0");
+            }
+            else if (leftY < 0 && leftY > -0.1) {
+            telemetry.addData("Left Stick Y", "In Dead Zone");
+        }
+            else {
+                telemetry.addData("Left Stick Y", "Is Zero");
+            }
+     */
