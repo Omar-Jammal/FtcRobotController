f95162d868eff39a711fd063e43d607602037cb2
vivsrobo <drrichadhamija@gmail.com>
2025-10-16 14:53:17 -0500

    Add ArcadeDrive mechanism and OpMode
    Introduces ArcadeDrive class for controlling four motors using arcade drive logic and ArcadeDriveOpMode for teleop control via gamepad. This enables simplified robot driving with throttle and spin inputs.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/mechanisms/ArcadeDriveOpMode.java
@@ -0,0 +1,22 @@
+package org.firstinspires.ftc.teamcode.mechanisms;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+public class ArcadeDriveOpMode extends OpMode {
+    ArcadeDrive drive = new ArcadeDrive();
+
+    double throttle, spin;
+    @Override
+    public void init(){
+        drive.init(hardwareMap);
+    }
+
+    @Override
+    public void loop() {
+        throttle = -gamepad1.left_stick_y;
+        spin = gamepad1.left_stick_x;
+
+        drive.drive(throttle, spin);;
+    }
+}
+
